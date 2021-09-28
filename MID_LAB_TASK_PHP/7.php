e45dc685c8101e3caf17a7465a06de8be5b6f0ad
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2021-09-28 17:15:03 +0600

    MID_LAB_TASK_PHP Updated

--- MID_LAB_TASK_PHP/7.php
+++ MID_LAB_TASK_PHP/7.php
@@ -10,4 +10,24 @@ function print_pattern($num)
 }
 $num = 3;
 print_pattern($num);
+
+echo "<br>";
+
+echo "<hr>";
+
+
+function pattern($n)
+{
+    $num = 65;
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = 0; $j <= $i; $j++) {
+            $ch = chr($num);
+            echo $ch . " ";
+            $num = $num + 1;
+        }
+        echo "<br>";
+    }
+}
+$n = 3;
+pattern($n);
 ?>
