407325389d2153d3a77985bf74b94991fe5cdd8f
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2021-10-07 00:20:39 +0600

    MID_LAB_TASK_PHP_FORM_2 Updated

--- MID_LAB_TASK_FORM_VALIDATION/Task-5/index.php
+++ MID_LAB_TASK_FORM_VALIDATION/Task-5/index.php
@@ -1,38 +1,72 @@
 <?php
 
-if (isset($_REQUEST['submit'])) {
-    $degree = $_REQUEST['myDegree'];
-    if ($degree != "") {
-        echo $degree;
+$Degree = "";
+$degreeError = "";
+
+
+if (($_SERVER["REQUEST_METHOD"] == "POST")) {
+    $degreeCount = 0;
+    if (!empty($_POST["degree"])) {
+        foreach ($_POST["degree"] as $select) {
+            $degreeCount++;
+        }
     } else {
-        echo "Null Value";
+        $degreeError = "At Least two boxes needs to be checked";
+    }
+
+    if (empty($_POST["degree"])) {
+        $degreeError = "No Degree Selected";
+    }
+    if ($degreeCount >= 2) {
+        $Degree = $_POST["degree"];
+    } else {
+        $degreeError = "At Least two boxes needs to be checked";
     }
 }
-
+function validateInput($information)
+{
+    $information = trim($information);
+    $information = stripslashes($information);
+    $information = htmlspecialchars($information);
+    return $information;
+}
 ?>
 
 <html>
 
 <head>
-    <title>DEGREE</title>
+    <title>Task-5</title>
 </head>
 
 <body>
-    <fieldset>
-        <legend>DEGREE</legend>
-        <form method="post">
-            <input type="checkbox" id="SSC" name="myDegree" value="SSC">
-            <label for="SSC">SSC</label>
-            <input type="checkbox" id="HSC" name="myDegree" value="HSC">
-            <label for="HSC">HSC</label>
-            <input type="checkbox" id="BSc" name="myDegree" value="BSc">
-            <label for="BSc">BSc</label>
-            <input type="checkbox" id="MSc" name="myDegree" value="MSc">
-            <label for="MSc">MSc</label>
-            <hr>
-            <input type="submit" name="submit" value="Submit">
-        </form>
-    </fieldset>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div>
+            <?php if ($degreeError == "") {
+                print_r($Degree);
+            }
+            ?>
+
+            <fieldset>
+                <legend><label>DEGREE&nbsp;</label></legend>
+
+                <input type="checkbox" id="degree" name="degree[]" value="SSC"> SSC
+                <input type="checkbox" id="degree" name="degree[]" value="HSC"> HSC
+                <input type="checkbox" id="degree" name="degree[]" value="BSc"> BSc
+                <input type="checkbox" id="degree" name="degree[]" value="MSc"> MSc <span class="red">
+                    <?php
+                    if ($degreeError != "") {
+                        echo "* - ";
+                        echo $degreeError;
+                    }
+                    ?>
+                </span>
+                <div style=margin-top:20px;>
+                    <input type="submit" value="Submit">
+                </div>
+            </fieldset>
+        </div>
+    </form>
+
 </body>
 
 </html>
